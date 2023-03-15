b178e1f418ca30c2edb5399948a2982a4b16b7da
Leo <hawkeye1607@gmail.com>
2023-03-15 11:08:36 +0100

    UserID wird erzeugt, siehe index.html

--- index.js
+++ index.js
@@ -1,36 +1,74 @@
+//Code erstellt userID für jede Sitzung, bei neuem Laden wird neue UserID erstellt
+//wenn der User keine ID hat, kann dieser nicht auf die users.html zugreifen -> muss sich erstmal eine ID holen
 const express = require("express");
-//const cookieParser = require("cookie-parser");
-var cookieSession = require('cookie-session')
+const { v4: uuid } = require("uuid");
+//const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const path = require('path');
 const app = express();
 
+const users = [];
+
+app.use(cookieParser());
+app.get("/", (req, res) => {
+  app.use("/", express.static("public"));
+  if(req.users === undefined) {
+    console.log("neuer User wurde erstellt")
+    const userID = Math.floor(Math.random() * 10000);
+    users.push(userID);
+    res.cookie("UserID", userID, {maxAge: 60*1000*60*60});
+    console.log(req.users);
+  }
+  console.log(users);
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+
+
+//app.use(express.static(__dirname + "/public"));
+//app.use("/", express.static("public"));
+
+/*app.get("/src/users", (req, res) => {
+  if(req.cookies.userID !== undefined) {
+    console.log("test");
+    console.log(JSON.stringify(users));
+    console.log(users.includes(req.cookies.userID));
+    res.send(users[req.cookies.userID] || []);
+  } else {
+    console.log("keine cookies");
+  }});
 
 /*
-app.use(cookieParser());
+
+app.get("/users/:id", (req, res) => {
+  const userID = req.params.id;
+  res.send(users.fond((user) => user.id === userID));
+});
+
+  if (req.cookies.userID !== undefined) {
+    if (users.includes(req.cookies.userID)) {
+      res.send("gut");
+    }
+    else {
+      const userID = Math.floor(Math.random() * 10000);
+      res.cookie("UserID", userID, {maxAge: 60*1000*60});
+    }}
+});
+
+app.get(cookieParser());
 app.use((req, _, next) => {
-  console.log(JSON.stringify(req.cookies));
+  console.debug(JSON.stringify(req.cookies));
   next();
-});
+});*/
 
-app.get("/anmelden", (_, res) => {
-  res.cookie("keks_z", 1, { maxAge: 120000 });
-  res.send("Anmeldung erfolgreich");
-});
+
+
+
+
+
+/*app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
 */
-app.use(cookieSession({
-  name: 'session_Denis',
-  keys: [0],
-
-  // Cookie Options
-  maxAge: 5 * 60 * 1000 // 24 hours
-}))
-
-
-app.use(express.static(__dirname + "/public"));
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
-});
 
 app.listen(8080);
 
